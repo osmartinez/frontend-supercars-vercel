5d1c4c4e7f5267fab2e02106cfe80afa8fb09c4f
osmartinez <its.osmartinez@gmail.com>
2024-06-20 20:05:22 +0200

    cambio url backend matias

--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class VehicleService {
 
-  url : string = "http://localhost:3000/api/vehicles"
+  url : string = "https://backend-supercars-vercel.vercel.app/api/vehicles"
   constructor(private http: HttpClient) { }
 
   getAll(){
